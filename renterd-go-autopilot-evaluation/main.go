628b60769f0482274c873baea5c29c32fac0b6ba
Alex Freska <alex.freska@gmail.com>
2024-04-23 10:07:39 -0400

    eval parameters

--- renterd-go-autopilot-evaluation/main.go
+++ renterd-go-autopilot-evaluation/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"reflect"
 
 	"github.com/joho/godotenv"
+	"go.sia.tech/core/types"
 	"go.sia.tech/renterd/autopilot"
 	"go.sia.tech/renterd/bus"
 )
@@ -25,7 +29,45 @@ func main() {
 	r, _ := b.RedundancySettings(ctx)
 	c, _ := a.Config()
 
+	// target
+	c.Contracts.Amount = 400
+
+	// gouging
+	g.MaxStoragePrice = types.Siacoins(5000)
+	g.MaxRPCPrice = types.Siacoins(1000)
+	g.MinMaxEphemeralAccountBalance = types.Siacoins(1)
+	// g.MinAccountExpiry = 1
+
+	// pruning
+	g.MaxDownloadPrice = types.Siacoins(1000)
+
+	// contract
+	// c.Contracts.Period = 10
+	// c.Contracts.RenewWindow = 10
+
 	e, _ := a.EvaluateConfig(ctx, c, g, r)
 
-	fmt.Printf("%+v\n", e)
+	fmt.Println("config:")
+	printStruct(c)
+	fmt.Println("gouging:")
+	printStruct(g)
+	fmt.Println("redundancy:")
+	printStruct(r)
+	fmt.Println("eval:")
+	printStruct(e)
+}
+
+func encodeJSON(w io.Writer, v any) error {
+	// encode nil slices as [] instead of null
+	if val := reflect.ValueOf(v); val.Kind() == reflect.Slice && val.Len() == 0 {
+		_, err := w.Write([]byte("[]\n"))
+		return err
+	}
+	enc := json.NewEncoder(w)
+	enc.SetIndent("", "  ")
+	return enc.Encode(v)
+}
+
+func printStruct(v any) error {
+	return encodeJSON(os.Stdout, v)
 }
--- renterd-js-bun-autopilot-evaluation/index.ts
+++ renterd-js-bun-autopilot-evaluation/index.ts
@@ -1,4 +1,5 @@
 import { Autopilot, Bus } from '@siafoundation/renterd-js'
+import { toHastings } from '@siafoundation/units'
 
 const api = Bun.env['RENTERD_API']!
 const password = Bun.env['RENTERD_PASSWORD']
@@ -13,16 +14,43 @@ const autopilot = Autopilot({
   password,
 })
 
-const g = await bus.settingGouging()
-const r = await bus.settingRedundancy()
-const c = await autopilot.config()
+try {
+  const g = await bus.settingGouging()
+  const r = await bus.settingRedundancy()
+  const c = await autopilot.config()
 
-const e = await autopilot.configEvaluate({
-  data: {
-    autopilotConfig: c.data,
-    gougingSettings: g.data,
-    redundancySettings: r.data,
-  },
-})
+  // target
+  c.data.contracts.amount = 500
 
-console.log(e.data)
+  // gouging
+  g.data.maxStoragePrice = toHastings(5000).toString()
+  g.data.maxRPCPrice = toHastings(1000).toString()
+  g.data.minMaxEphemeralAccountBalance = toHastings(1).toString()
+  // g.data.minAccountExpiry = 1
+
+  // pruning
+  g.data.maxDownloadPrice = toHastings(1000).toString()
+
+  // contract
+  // c.data.contracts.period = 10
+  // c.data.contracts.renewWindow = 10
+
+  const e = await autopilot.configEvaluate({
+    data: {
+      autopilotConfig: c.data,
+      gougingSettings: g.data,
+      redundancySettings: r.data,
+    },
+  })
+
+  console.log('config:')
+  console.log(c.data)
+  console.log('gouging:')
+  console.log(g.data)
+  console.log('redundancy:')
+  console.log(r.data)
+  console.log('eval:')
+  console.log(e.data)
+} catch (e) {
+  console.log(e)
+}
